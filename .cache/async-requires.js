8cffeac7063f62f2a5fc1c1d1867c4fe004540a2
Eric Flatt <eflatt18@gmail.com>
2020-03-18 23:27:07 -0400

    startup and comtion added

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -2,13 +2,15 @@
 const preferDefault = m => m && m.default || m
 
 exports.components = {
-  "component---cache-dev-404-page-js": () => import("./dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
+  "component---node-modules-gatsby-plugin-offline-app-shell-js": () => import("./../node_modules/gatsby-plugin-offline/app-shell.js" /* webpackChunkName: "component---node-modules-gatsby-plugin-offline-app-shell-js" */),
   "component---src-pages-bond-jsx": () => import("./../src/pages/bond.jsx" /* webpackChunkName: "component---src-pages-bond-jsx" */),
+  "component---src-pages-comotion-jsx": () => import("./../src/pages/comotion.jsx" /* webpackChunkName: "component---src-pages-comotion-jsx" */),
   "component---src-pages-floatie-jsx": () => import("./../src/pages/floatie.jsx" /* webpackChunkName: "component---src-pages-floatie-jsx" */),
   "component---src-pages-google-jsx": () => import("./../src/pages/google.jsx" /* webpackChunkName: "component---src-pages-google-jsx" */),
   "component---src-pages-index-jsx": () => import("./../src/pages/index.jsx" /* webpackChunkName: "component---src-pages-index-jsx" */),
   "component---src-pages-new-landing-jsx": () => import("./../src/pages/newLanding.jsx" /* webpackChunkName: "component---src-pages-new-landing-jsx" */),
   "component---src-pages-obo-jsx": () => import("./../src/pages/obo.jsx" /* webpackChunkName: "component---src-pages-obo-jsx" */),
+  "component---src-pages-startup-jsx": () => import("./../src/pages/startup.jsx" /* webpackChunkName: "component---src-pages-startup-jsx" */),
   "component---src-pages-test-jsx": () => import("./../src/pages/test.jsx" /* webpackChunkName: "component---src-pages-test-jsx" */),
   "component---src-pages-web-design-jsx": () => import("./../src/pages/webDesign.jsx" /* webpackChunkName: "component---src-pages-web-design-jsx" */)
 }
