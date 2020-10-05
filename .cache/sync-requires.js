0b120f7a9b4b4581a29e9de9e9549abb7caf17bc
ericflatt <eflatt18@gmail.com>
2020-10-05 14:39:40 -0400

    updates

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,16 +5,16 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---node-modules-gatsby-plugin-offline-app-shell-js": hot(preferDefault(require("C:\\Users\\eflat\\Documents\\gatsby-portfolio\\node_modules\\gatsby-plugin-offline\\app-shell.js"))),
-  "component---src-pages-bond-jsx": hot(preferDefault(require("C:\\Users\\eflat\\Documents\\gatsby-portfolio\\src\\pages\\bond.jsx"))),
-  "component---src-pages-comotion-jsx": hot(preferDefault(require("C:\\Users\\eflat\\Documents\\gatsby-portfolio\\src\\pages\\comotion.jsx"))),
-  "component---src-pages-floatie-jsx": hot(preferDefault(require("C:\\Users\\eflat\\Documents\\gatsby-portfolio\\src\\pages\\floatie.jsx"))),
-  "component---src-pages-google-jsx": hot(preferDefault(require("C:\\Users\\eflat\\Documents\\gatsby-portfolio\\src\\pages\\google.jsx"))),
-  "component---src-pages-index-jsx": hot(preferDefault(require("C:\\Users\\eflat\\Documents\\gatsby-portfolio\\src\\pages\\index.jsx"))),
-  "component---src-pages-new-landing-jsx": hot(preferDefault(require("C:\\Users\\eflat\\Documents\\gatsby-portfolio\\src\\pages\\newLanding.jsx"))),
-  "component---src-pages-obo-jsx": hot(preferDefault(require("C:\\Users\\eflat\\Documents\\gatsby-portfolio\\src\\pages\\obo.jsx"))),
-  "component---src-pages-startup-jsx": hot(preferDefault(require("C:\\Users\\eflat\\Documents\\gatsby-portfolio\\src\\pages\\startup.jsx"))),
-  "component---src-pages-test-jsx": hot(preferDefault(require("C:\\Users\\eflat\\Documents\\gatsby-portfolio\\src\\pages\\test.jsx"))),
-  "component---src-pages-web-design-jsx": hot(preferDefault(require("C:\\Users\\eflat\\Documents\\gatsby-portfolio\\src\\pages\\webDesign.jsx")))
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/ericflatt/gatsby-portfolio/.cache/dev-404-page.js"))),
+  "component---src-pages-bond-jsx": hot(preferDefault(require("/Users/ericflatt/gatsby-portfolio/src/pages/bond.jsx"))),
+  "component---src-pages-comotion-jsx": hot(preferDefault(require("/Users/ericflatt/gatsby-portfolio/src/pages/comotion.jsx"))),
+  "component---src-pages-floatie-jsx": hot(preferDefault(require("/Users/ericflatt/gatsby-portfolio/src/pages/floatie.jsx"))),
+  "component---src-pages-google-jsx": hot(preferDefault(require("/Users/ericflatt/gatsby-portfolio/src/pages/google.jsx"))),
+  "component---src-pages-index-jsx": hot(preferDefault(require("/Users/ericflatt/gatsby-portfolio/src/pages/index.jsx"))),
+  "component---src-pages-new-landing-jsx": hot(preferDefault(require("/Users/ericflatt/gatsby-portfolio/src/pages/newLanding.jsx"))),
+  "component---src-pages-obo-jsx": hot(preferDefault(require("/Users/ericflatt/gatsby-portfolio/src/pages/obo.jsx"))),
+  "component---src-pages-startup-jsx": hot(preferDefault(require("/Users/ericflatt/gatsby-portfolio/src/pages/startup.jsx"))),
+  "component---src-pages-test-jsx": hot(preferDefault(require("/Users/ericflatt/gatsby-portfolio/src/pages/test.jsx"))),
+  "component---src-pages-web-design-jsx": hot(preferDefault(require("/Users/ericflatt/gatsby-portfolio/src/pages/webDesign.jsx")))
 }
 
